0c8ed9b49a9aea6412bcde9e7c49a921cad3c8b1
HyeonChae1104 <129866491+HyeonChae1104@users.noreply.github.com>
2024-01-16 12:23:58 +0900

    [level Lv.0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,11 @@
+function solution(n, k) {
+    var answer = 0;
+    if(n<10){
+        answer = 12000*n + 2000*k
+    }
+    else if(n>=10){
+        answer = 12000*n + 2000*(k-Math.floor(n/10))
+    }
+    
+    return answer;
+}
