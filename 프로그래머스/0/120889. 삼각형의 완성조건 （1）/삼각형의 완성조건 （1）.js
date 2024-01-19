87b5eb9b848cf59b6cacdf5f055f11da78f73cfc
HyeonChae1104 <129866491+HyeonChae1104@users.noreply.github.com>
2024-01-19 23:49:39 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,12 @@
+function solution(sides) {
+    var answer = 0;
+    sides.length = 3;
+    sides.sort((a, b) => a - b);
+    if(sides[2]< sides[0]+ sides[1]){
+        answer = 1;
+    }
+    else{
+        answer = 2;
+    }
+    return answer;
+}
