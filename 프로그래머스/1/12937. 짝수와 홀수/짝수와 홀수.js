7ac8e74e3d4d475f08781e874096ba453feffd2f
HyeonChae1104 <129866491+HyeonChae1104@users.noreply.github.com>
2024-01-19 23:26:21 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,11 @@
+function solution(num) { 
+    var answer = '';
+    if(num%2 == 0){
+        answer = "Even"
+    }
+    else if(num%2 != 0){
+        answer = "Odd"
+    }
+   
+    return answer;
+}
