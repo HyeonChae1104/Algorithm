064338bf62e817aca579d185d573d91682431c5d
HyeonChae1104 <129866491+HyeonChae1104@users.noreply.github.com>
2024-01-22 00:50:31 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0; // 약수의 합
+    for(var i=0; i<=n; i++){
+        if(n%i ==0){
+            answer += i
+        }
+        else{
+            answer 
+        }
+    }
+    return answer;
+}
