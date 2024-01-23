75ecb8f06ea363da010d5939e7d49c5b6d03603c
HyeonChae1104 <129866491+HyeonChae1104@users.noreply.github.com>
2024-01-23 16:00:36 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -0,0 +1,9 @@
+function solution(start, end_num) {
+    var answer = [];
+    var j = 0;
+    for(start; start>=end_num; start--){
+            answer.push(start)
+        }
+    
+    return answer;
+}
