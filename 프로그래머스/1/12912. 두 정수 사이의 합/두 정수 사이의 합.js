501b759af86371c7c6b1b99697a966be381ba774
HyeonChae1104 <129866491+HyeonChae1104@users.noreply.github.com>
2024-01-25 10:31:31 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,16 @@
+function solution(a, b) {
+    var answer = 0;
+    
+    if(a<=b){
+        for(var i=a; i<=b; i++){
+            answer += i;
+    }  
+}
+    else if(a>b){
+        for(var i=b; i<=a; i++){
+            answer += i;
+      
+    }
+}
+    return answer;
+}
