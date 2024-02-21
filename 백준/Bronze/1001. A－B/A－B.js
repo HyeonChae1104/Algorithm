5d61b47bc6423c3e41da0c4f915a67f1e9c6f241
HyeonChae1104 <129866491+HyeonChae1104@users.noreply.github.com>
2024-02-21 10:26:43 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const inputdata = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+const A = parseInt(inputdata[0]);
+const B = parseInt(inputdata[1]);
+
+console.log(A-B);
