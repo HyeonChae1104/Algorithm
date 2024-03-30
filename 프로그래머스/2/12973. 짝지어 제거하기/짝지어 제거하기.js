ad1d8d2e4f1d11627f1e1622adb73168d20101b2
HyeonChae1104 <129866491+HyeonChae1104@users.noreply.github.com>
2024-03-30 21:38:20 +0900

    [level 2] Title: 짝지어 제거하기, Time: 51.41 ms, Memory: 46.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,18 @@
+function solution(s){
+    let stack = [];
+    
+    for(var i=0; i<s.length; i++){
+        if(stack.length > 0 && stack[stack.length - 1] == s[i]){
+            stack.pop();
+        }
+        else{
+            stack.push(s[i]);
+        }
+    }
+        
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+
+    return stack.length == 0? 1:0;
+}
